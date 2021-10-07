7e38abce3ef67b8269aaf88f14b431a9c03c9516
Isaac <iketart2@gmail.com>
2021-10-07 00:12:22 -0700

    CRUD routes

--- server/Routes/apiRoutes.js
+++ server/Routes/apiRoutes.js
@@ -2,15 +2,46 @@ const express = require('express');
 const router = express.Router();
 const RelatedAPI = require('../Models/apiModel.js');
 
-// Retrieve Product ID & Related Product ID
-router.get('/related', (req,res) => {
 
-let productID = req.query._id
+
+
+
+// POST
+router.post('/related', (req,res) => {
+ 
+    RelatedAPI.insertOne(req.body)
+    .then(result => {
+      console.log(result)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+    
+    RelatedAPI.find({"_id" : productID})
+      .then((pid) => {
+        const relatedArrayIds = []
+        pid[0].related_product_id.map(data => {
+         if (!relatedArrayIds.includes(data)) {
+          relatedArrayIds.push(data)
+         }
+        })
+        res.send(relatedArrayIds);
+      })
+  });
+
+
+
+
+
+
+
+// GET
+router.get('/related', (req,res) => {
+let  = req.query._id
   //if no _id is added defaults to first item in database
   if (req.query._id === undefined) {
     productID = 1
   }
-
     process.on('uncaughtException', function(ex) {
       res.end('ID does not exist')
     })
@@ -24,8 +55,69 @@ let productID = req.query._id
        }
       })
       res.send(relatedArrayIds);
-    });
+    })
 });
+
+
+
+
+
+// PUT
+router.put('/related', (req,res) => {
+  let  = req.query._id
+    //if no _id is added defaults to first item in database
+    if (req.query._id === undefined) {
+      productID = 1
+    }
+      process.on('uncaughtException', function(ex) {
+        res.end('ID does not exist')
+      })
+  
+    RelatedAPI.find({"_id" : productID})
+      .then((pid) => {
+        const relatedArrayIds = []
+        pid[0].related_product_id.map(data => {
+         if (!relatedArrayIds.includes(data)) {
+          relatedArrayIds.push(data)
+         }
+        })
+        res.send(relatedArrayIds);
+      })
+  });
+
+
+
+
+
+
+
+// DELETE
+router.delete('/related', (req,res) => {
+  let  = req.query._id
+    //if no _id is added defaults to first item in database
+    if (req.query._id === undefined) {
+      productID = 1
+    }
+      process.on('uncaughtException', function(ex) {
+        res.end('ID does not exist')
+      })
+  
+    RelatedAPI.find({"_id" : productID})
+      .then((pid) => {
+        const relatedArrayIds = []
+        pid[0].related_product_id.map(data => {
+         if (!relatedArrayIds.includes(data)) {
+          relatedArrayIds.push(data)
+         }
+        })
+        res.send(relatedArrayIds);
+      })
+  });
+
+
+
+
+
 
 module.exports = router
 
